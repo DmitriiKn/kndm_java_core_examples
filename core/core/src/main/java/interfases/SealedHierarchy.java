bc8f6adcaffe90a2f616b2f508a81655fced2a0c
DmitriiKn <dmitriikn250@gmail.com>
2024-01-19 22:33:55 +0300

    sealed interfaces and classes

---
+++ core/core/src/main/java/interfases/SealedHierarchy.java
@@ -0,0 +1,34 @@
+package interfases;
+
+public class SealedHierarchy {
+}
+
+sealed interface Shape permits Qube, Triangle{
+    void draw();
+}
+
+non-sealed interface Qube extends Shape{
+
+}
+
+sealed interface Triangle extends Shape permits DoubleTriangle{
+
+}
+
+non-sealed class DoubleTriangle implements Triangle {
+    @Override
+    public void draw() {
+    }
+}
+
+
+/**
+ * Hierarchy error here because Triangle doesn't permit to TripleTriangle
+ */
+non-sealed class TripleTriangle implements Triangle {
+    @Override
+    public void draw() {
+    }
+}
+
+
